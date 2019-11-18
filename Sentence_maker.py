b1679ed5e15addce4d60c35876ddef194a257c5c
Nibs90 <jarska90@gmail.com>
2019-11-18 20:05:21 +0200

    Made comments

--- Sentence_maker.py
+++ Sentence_maker.py
@@ -1,3 +1,7 @@
+#This ones check if there is a question-word in a sentence
+#if there's, it'll add a questionmark in the end of the sentence
+#if not, nothing will change
+
 def sentence_maker(phrase):
     questionWords = ("how", "why", "when", "what")
     low = phrase.lower()
