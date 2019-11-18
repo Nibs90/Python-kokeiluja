5150a04f2074f54d61a601a049c54d268eab9371
Nibs90 <jarska90@gmail.com>
2019-11-18 20:20:52 +0200

    More changes by me

--- Sentence_maker.py
+++ Sentence_maker.py
@@ -1,6 +1,10 @@
-#This ones check if there is a question-word in a sentence
-#if there's, it'll add a questionmark in the end of the sentence
-#if not, nothing will change
+# Based on the-python-mega-course in Udemy
+# This ones check if there is a question-word in a sentence
+# if there's, it'll add a questionmark in the end of the sentence
+# if not, nothing will change
+# adding sentence to showing later
+# all sentences will start with a uppercase letter.
+
 
 def sentence_maker(phrase):
     questionWords = ("how", "why", "when", "what")
@@ -10,3 +14,14 @@ def sentence_maker(phrase):
         return "{}?".format(capitalized)
     else:
         return "{}.".format(capitalized)
+
+
+results = []
+while True:
+    syote = input("Say something: ")
+    if syote == "/exit":
+        break
+    else:
+        results.append(sentence_maker(syote))
+
+print(" ".join(results))
